74338db869c298d79ed3903f669b19e3498be14e
“emilesherrott” <emilesherrott@gmail.com>
2023-02-07 11:02:19 +0000

    database seeded

---
+++ server/db/connection.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv").config()
+const { Pool } = require("pg")
+
+const client = new Pool({
+    connectionString: process.env.DBCONNECTIONSTRING
+})
+
+
+module.exports = client
---
+++ server/db/setUp.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+const client = require("./connection")
+
+const countriesSchema = fs.readFileSync('./db/countries.sql').toString()
+const seedData = fs.readFileSync('./db/seed.sql').toString()
+
+const setUpDB = async () => {
+    await client.query(countriesSchema)
+    await client.query(seedData)
+    console.log("Database ready")
+}
+
+setUpDB()
